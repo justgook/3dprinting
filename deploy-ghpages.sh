f8107489e132b5fb6f4d54974907c35b6f137c4c
Roman Potashow <justgook@gmail.com>
2015-02-09 12:52:33 +0200

    Fixed build

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 rm -rf build || exit 0;
 #mkdir out;
-./node_modules/wintersmith/bin/wintersmith buil
-( cd build
+./node_modules/wintersmith/bin/wintersmith build || exit 0;
+( cd build || exit 0;
  git init
  git config user.name "Travis-CI"
  git config user.email "travis@nodemeatspace.com"
- #cp ../CNAME ./CNAME
- #cp ../countryiso.js ./countryiso.js
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
