2612969e65802b4688324c3a239329abdfe9a0fb
Roman Potashow <justgook@gmail.com>
2015-02-09 12:56:42 +0200

    fixed custom name

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 rm -rf build || exit 0;
-#mkdir out;
+mkdir build;
 ./node_modules/wintersmith/bin/wintersmith build || exit 0;
 ( cd build || exit 0;
  git init
  git config user.name "Travis-CI"
  git config user.email "travis@nodemeatspace.com"
+ cp ../CNAME ./CNAME
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
